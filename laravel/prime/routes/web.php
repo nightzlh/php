11c5ca2540c80cbbe1ce0714414f4d12194a914a
zhongliheng <zhongliheng@shoyoo.com>
2019-07-30 15:38:05 +0800

    add something about validateion middleware

--- laravel/prime/routes/web.php
+++ laravel/prime/routes/web.php
@@ -15,7 +15,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+/*
+| This route is for hello world example
+*/
+
 Route::get('user/{id}', 'UserController@show');
+
+/*
+| This route is for db connection example
+*/
 
 Route::get('dbUser/{id}', 'UserController@showProfile');
 
+/*
+| These routes is used for validation example
+*/
+
+Route::get('post/create', 'PostController@create');
+
+Route::post('post', 'PostController@store');
